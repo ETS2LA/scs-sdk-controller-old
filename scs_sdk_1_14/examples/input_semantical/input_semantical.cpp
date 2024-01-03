a26481f6e63f2d1bd90ebe04dc5bf4960c398572
Tumppi066 <tuomas.keidasto@gmail.com>
2024-01-03 21:27:14 +0200

    Update

--- scs_sdk_1_14/examples/input_semantical/input_semantical.cpp
+++ scs_sdk_1_14/examples/input_semantical/input_semantical.cpp
@@ -80,8 +80,12 @@ void log_line(const char* const text, ...)
 #include "amtrucks/scssdk_input_ats.h"
 
 // Shared Memory
+const int axisCount = 4;
+const int buttonCount = 15;
 const wchar_t* memname = L"Local\\SCSControls";
-const size_t size = 3 * sizeof(float);
+const size_t buttonSize = buttonCount * sizeof(bool);
+const size_t axisSize = axisCount * sizeof(float);
+const size_t size = axisSize + buttonSize;
 HANDLE hMapFile;
 
 // Function to initialize shared memory
@@ -112,7 +116,18 @@ void initialize_mem() {
 		return;
 	}
 
-	float data[3] = { 0.0, 0.0, 0.0 };
+	float data[buttonCount + axisCount] = {};
+	for (int i = 0; i < buttonCount + axisCount; i++)
+	{
+		if (i < axisCount)
+		{
+			data[i] = 0.0;
+		}
+		else
+		{
+			data[i] = false;
+		}
+	}
 	memcpy(pBuf, data, size);
 
 	UnmapViewOfFile(pBuf);
@@ -121,25 +136,29 @@ void initialize_mem() {
 }
 
 // Function to read shared memory
-std::array<float, 3> read_mem() {
+std::pair<std::array<float, axisCount>, std::array<bool, buttonCount>> read_mem() {
 	if (hMapFile == NULL) {
 		log_line("Shared mem file not open.");
-		return std::array<float, 3>{};
+		return std::make_pair(std::array<float, axisCount>{}, std::array<bool, buttonCount>{});
 	}
 
 	void* pBuf = MapViewOfFile(hMapFile, FILE_MAP_ALL_ACCESS, 0, 0, size);
 
-	std::array<float, 3> data;
-	memcpy(data.data(), pBuf, size);
+	std::array<float, axisCount> floatData;
+	std::array<bool, buttonCount> boolData;
+
+	memcpy(floatData.data(), pBuf, axisSize);
+	memcpy(boolData.data(), static_cast<char*>(pBuf) + size, buttonSize);
 
 	UnmapViewOfFile(pBuf);
-	return data;
+	return std::make_pair(floatData, boolData);
 }
 
 #define UNUSED(x)
 
 int inputNumber = 0;
-scs_float_t inputValues[3];
+scs_float_t inputValues[4];
+scs_value_bool_t inputBools[15];
 scs_input_device_t device_info;
 SCSAPI_RESULT input_event_callback(scs_input_event_t* const event_info, const scs_u32_t flags, const scs_context_t UNUSED(context))
 {
@@ -155,64 +174,43 @@ SCSAPI_RESULT input_event_callback(scs_input_event_t* const event_info, const sc
 	if (SCS_INPUT_EVENT_CALLBACK_FLAG_first_in_frame == 1 && inputNumber == 0) {
 		inputNumber = 0;
 
-		std::array<float, 3> values = read_mem();
+		// Read the floats from shared memory
+		std::pair<std::array<float, axisCount>, std::array<bool, buttonCount>> data = read_mem();
+		std::array<float, axisCount> values = data.first;
+		std::array<bool, buttonCount> bools = data.second;
 
-		// Check that none of the values are too high or too low.
-		if (values[0] > 1) 
+		for (int i = 0; i < axisCount; i++)
 		{
-			values[0] = 1;
-		}
-		if (values[0] < -1)
-		{
-			values[0] = -1;
-		}
-		if (values[1] > 1)
-		{
-			values[1] = 1;
-		}
-		if (values[1] < -1)
-		{
-			values[1] = -1;
-		}
-		if (values[2] > 1)
-		{
-			values[2] = 1;
-		}
-		if (values[2] < -1)
-		{
-			values[2] = -1;
-		}
-
-		char buffer[64];
-		int ret = snprintf(buffer, sizeof buffer, "%f", values[0]);
-
-		for (int i = 0; i < device_info.input_count; i++) {
+			if (values[i] > 1.0) {
+				values[i] = 1.0;
+			}
+			if (values[i] < -1.0) {
+				values[i] = -1.0;
+			}
 			inputValues[i] = values[i];
 		}
 
-		if (ret < 0) {
-			return EXIT_FAILURE;
+		// Set the bools
+		for (int i = 0; i < buttonCount; i++)
+		{
+			inputBools[i].value = bools[i];
 		}
-		
-		log_line(buffer);
 	}
 
 	event_info->input_index = inputNumber;
 
-	if (inputNumber == 0)
+	if (inputNumber < axisCount)
 	{
-		event_info->value_float.value = inputValues[0];
-		log_line("Updated steering");
+		event_info->value_float.value = inputValues[inputNumber];
 	}
-	if (inputNumber == 1)
+	else
 	{
-		event_info->value_float.value = inputValues[1];
-		log_line("Updated acceleration");
-	}
-	if (inputNumber == 2)
-	{
-		event_info->value_float.value = inputValues[2];
-		log_line("Updated braking");
+		// TODO: This code is not running, well atleast the value is not true.
+		if (inputBools[inputNumber].value == true)
+		{
+			log_line("Button %d pressed", inputNumber);
+		}
+		event_info->value_bool.value = inputBools[inputNumber].value;
 	}
 
 	inputNumber++;
@@ -247,12 +245,50 @@ SCSAPI_RESULT scs_input_init(const scs_u32_t version, const scs_input_init_param
 	device_info.display_name = "ETS2 Lane Assist";
 	device_info.type = SCS_INPUT_DEVICE_TYPE_semantical;
 
+	// These are all the somewhat useful commands from the controls
+	// Name, Index, Type, Control Name In File
+	// Steering, 0, float, steering
+	// Acceleration, 1, float, aforward
+	// Braking, 2, float, abackward
+	// Clutch, 3, float, clutch
+	// Pause Game, 4, bool,
+	// Parking Brake, 5, bool, parkingbrake
+	// Wipers, 6, bool, wipers
+	// Cruise Control, 7, bool, cruiectrl
+	// Cruise Control Increase, 8, bool, cruiectrlinc
+	// Cruise Control Decrease, 9, bool, cruiectrldec
+	// Cruise Control Reset, 10, bool, cruiectrlres
+	// Lights, 11, bool, light
+	// High Beams, 12, bool, hblight
+	// Left Blinker, 13, bool, lblinker
+	// Right Blinker, 14, bool, rblinker
+	// Quickpark, 15, bool, quickpark
+	// Drive(Gear), 16, bool, drive
+	// Reverse(Gear), 17, bool, reverse
+	// Cycle Zoom(map ? ), 18, bool, cycl_zoom
+
 	const scs_input_device_input_t inputs[] = { 
 		{"steering", "ETS2LA Steering", SCS_VALUE_TYPE_float }, 
 		{"aforward", "ETS2LA Forward", SCS_VALUE_TYPE_float }, 
-		{"abackward", "ETS2LA Backward", SCS_VALUE_TYPE_float } 
+		{"abackward", "ETS2LA Backward", SCS_VALUE_TYPE_float },
+		{"clutch", "ETS2LA Clutch", SCS_VALUE_TYPE_float },
+		{"pause", "ETS2LA Pause", SCS_VALUE_TYPE_bool },
+		{"parkingbrake", "ETS2LA Parking Brake", SCS_VALUE_TYPE_bool },
+		{"wipers", "ETS2LA Wipers", SCS_VALUE_TYPE_bool },
+		{"cruiectrl", "ETS2LA Cruise Control", SCS_VALUE_TYPE_bool },
+		{"cruiectrlinc", "ETS2LA Cruise Control Increase", SCS_VALUE_TYPE_bool },
+		{"cruiectrldec", "ETS2LA Cruise Control Decrease", SCS_VALUE_TYPE_bool },
+		{"cruiectrlres", "ETS2LA Cruise Control Reset", SCS_VALUE_TYPE_bool },
+		{"light", "ETS2LA Lights", SCS_VALUE_TYPE_bool },
+		{"hblight", "ETS2LA High Beams", SCS_VALUE_TYPE_bool },
+		{"lblinker", "ETS2LA Left Blinker", SCS_VALUE_TYPE_bool },
+		{"rblinker", "ETS2LA Right Blinker", SCS_VALUE_TYPE_bool },
+		{"quickpark", "ETS2LA Quickpark", SCS_VALUE_TYPE_bool },
+		{"drive", "ETS2LA Drive", SCS_VALUE_TYPE_bool },
+		{"reverse", "ETS2LA Reverse", SCS_VALUE_TYPE_bool },
+		{"cycl_zoom", "ETS2LA Cycle Zoom", SCS_VALUE_TYPE_bool },	
 	};
-	device_info.input_count = 3;
+	device_info.input_count = axisCount + buttonCount;
 	device_info.inputs = inputs;
 
 	device_info.input_event_callback = input_event_callback;
--- scs_sdk_1_14/examples/input_semantical/test.py
+++ scs_sdk_1_14/examples/input_semantical/test.py
@@ -6,11 +6,17 @@ import time
 # Open the file
 mmName = r"Local\SCSControls"
 
+floatCount = 3
+boolCount = 16
+
 # Memory map the file
 print("Waiting for memory map from game...")
 buf = None
 while buf is None:
-    buf = mmap.mmap(0, 3 * 4, "Local\SCSControls")  # 3 floats, 4 bytes each
+    floatSize = floatCount * 4
+    boolSize = boolCount * 1
+    size = floatSize + boolSize
+    buf = mmap.mmap(0, size, "Local\SCSControls")  # 3 floats, 4 bytes each
     time.sleep(0.1)
 print("Memory map received!")
 
@@ -18,8 +24,12 @@ steering = -1
 didIncrease = False
 try:
     while True:
-        # Write three floats to memory
-        buf[:] = struct.pack('fff', steering, 0.0, 0.0)
+        # Write the floats and bools to memory
+        buf[:] = struct.pack('ffff15?', steering, 0.0, 0.0, 0.0,
+                             False, False, False, False,
+                             False, False, False, False,
+                             False, False, False, False,
+                             False, False, False)
 
         # Sleep for a while to prevent high CPU usage
         time.sleep(0.01)
@@ -33,4 +43,6 @@ try:
             didIncrease = False
         print(steering, end='\r')
 except:
+    import traceback
+    traceback.print_exc()
     pass
